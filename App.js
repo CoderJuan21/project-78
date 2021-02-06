e6f9ff47912ef2ebc169196169bd3f86d6b864a8
CoderJuan21 <70552194+CoderJuan21@users.noreply.github.com>
2021-02-06 14:48:36 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignUpLoginScreen from './screens/SignUpLoginScreen';
+
+export default function App() {
+  return (
+    <SignUpLoginScreen/>
+  )
+}
